cfa0055cdf7a39098aa3d536c8d35e00f2c1528b
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-02-01 09:03:16 +0200

    Initial Commit

---
+++ ScreenRecordingYourAppInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScreenRecordingYourAppInSwiftUI
+//
+//  Created by ramil on 01.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScreenRecordingYourAppInSwiftUI/ScreenRecordingYourAppInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenRecordingYourAppInSwiftUIApp.swift
+//  ScreenRecordingYourAppInSwiftUI
+//
+//  Created by ramil on 01.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScreenRecordingYourAppInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
